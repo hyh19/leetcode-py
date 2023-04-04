c07a0657f3298fce76517d7291f3d85dadf6d797
e7e26e5 <abc@xyz.com>
2023-04-04 23:46:52 +0800

    update

--- main.py
+++ main.py
@@ -2,20 +2,44 @@ from typing import List
 
 
 class Solution:
-    def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        clips.sort(key=lambda clip: (clip[0], -clip[1]))
-        count = 0
-        cover_end = 0
-        i, n = 0, len(clips)
-        # 当 clips[i] 与 [0, cover_end] 重叠（相交或被覆盖）时
-        while i < n and clips[i][0] <= cover_end:
-            # 记录与 [0, cover_end] 重叠的所有区间中最大的 end
-            max_end = 0
-            while i < n and clips[i][0] <= cover_end:
-                max_end = max(max_end, clips[i][1])
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        return self.kSum(3, nums, 0, len(nums) - 1, 0)
+
+    # 返回升序子数组 nums[lo..hi] 中所有和为 target 且不重复的 k 元组
+    def kSum(self, k: int, nums: List[int], lo: int, hi: int, target: int) -> List[List[int]]:
+        if hi - lo + 1 < k:
+            return []
+        if k == 2:
+            return self.twoSum(nums, lo, hi, target)
+        res = []
+        i = lo
+        while i <= hi:
+            curNum = nums[i]
+            sp = self.kSum(k - 1, nums, i + 1, hi, target - curNum)
+            for x in sp:
+                x.append(curNum)
+                res.append(x)
+            while i <= hi and nums[i] == curNum:
                 i += 1
-            count += 1
-            cover_end = max_end
-            if cover_end >= time:
-                return count
-        return -1
+        return res
+
+    # 返回升序子数组 nums[lo..hi] 中所有和为 target 且不重复的 2 元组
+    def twoSum(self, nums: List[int], lo: int, hi: int, target: int) -> List[List[int]]:
+        res = []
+        while lo < hi:
+            first, second = nums[lo], nums[hi]
+            sum = first + second
+            if sum < target:
+                while lo < hi and nums[lo] == first:
+                    lo += 1
+            elif sum > target:
+                while lo < hi and nums[hi] == second:
+                    hi -= 1
+            else:
+                res.append([first, second])
+                while lo < hi and nums[lo] == first:
+                    lo += 1
+                while lo < hi and nums[hi] == second:
+                    hi -= 1
+        return res
